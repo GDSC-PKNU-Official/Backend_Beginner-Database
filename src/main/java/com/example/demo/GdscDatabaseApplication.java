356a47f5a2e686a74415335240c2a7802a6a4bec
o-jeong <ljj020304@gmail.com>
2024-07-13 19:02:12 +0900

    init

---
+++ src/main/java/com/example/demo/GdscDatabaseApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GdscDatabaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GdscDatabaseApplication.class, args);
+	}
+
+}
